1d992c96662f0e33fb2dc01d87c604a0f723f38c
mov <mov51@mov51.net>
2022-11-24 01:51:37 -0500

    Add toggle

--- src/main/java/net/mov51/serverstone/utils/Targets.java
+++ src/main/java/net/mov51/serverstone/utils/Targets.java
@@ -1,46 +1,59 @@
 package net.mov51.serverstone.utils;
 
+import net.mov51.serverstone.ServerStone;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Powerable;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Targets {
     //list of block targets
-    private static HashSet<Location> targets = new HashSet<>();
+    private List<Location> targetedBlocks = new ArrayList<>();
 
     public Targets(){
         //constructor
     }
 
     public void add(Location location){
-        targets.add(location);
+        this.targetedBlocks.add(location);
         System.out.println("Added target: " + location);
     }
+
     public void remove(Location location){
-        targets.remove(location);
+        this.targetedBlocks.remove(location);
         System.out.println("Removed target: " + location);
     }
+
     public void clear(){
-        targets.clear();
+        this.targetedBlocks.clear();
         System.out.println("Targets cleared");
     }
+
     public void powerAll(){
-        for(Location blockL : targets){
+        for(Location blockL : this.targetedBlocks){
             setPowerState(blockL,true);
         }
     }
 
     public void dePowerAll(){
-        for(Location blockL : targets){
+        for(Location blockL : this.targetedBlocks){
             setPowerState(blockL,false);
         }
     }
+
+    public void toggleAll(){
+        for(Location blockL : this.targetedBlocks){
+            togglePowerState(blockL);
+        }
+    }
+
     public boolean contains(Location location){
-        return targets.contains(location);
+        return this.targetedBlocks.contains(location);
     }
 
     private void setPowerState(Location location, boolean state){
@@ -49,6 +62,20 @@ public class Targets {
             if(blockData instanceof Powerable){
                 Powerable powerable = (Powerable) blockData;
                 powerable.setPowered(state);
+                location.getBlock().setBlockData(powerable);
+            }
+        }else{
+            System.out.println("Target is not a lever: " + location);
+            this.remove(location);
+        }
+    }
+
+    private void togglePowerState(Location location){
+        if(location.getBlock().getType() == Material.LEVER){
+            BlockData blockData = location.getBlock().getBlockData();
+            if(blockData instanceof Powerable){
+                Powerable powerable = (Powerable) blockData;
+                powerable.setPowered(!powerable.isPowered());
                 location.getBlock().setBlockData(powerable);
             }
         }else{
